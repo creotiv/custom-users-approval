d5393a10540faf199f01093f3ec91a54ab1f0c8d
Andrey Nikishaev <creotiv@gmail.com>
2024-02-22 17:57:33 +0200

    first release

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,60 +1,60 @@
 import * as core from '@actions/core'
-import { Context } from '@actions/github/lib/context';
-import { run } from '../src/main';
-import { Octokit } from '@octokit/rest';
+import { Context } from '@actions/github/lib/context'
+import { run } from '../src/main'
+import { Octokit } from '@octokit/rest'
 
 jest.mock('@actions/core', () => ({
-    getInput: jest.fn(),
-    setFailed: jest.fn(),
-    info: jest.fn(),
-    debug: jest.fn(),
-    startGroup: jest.fn(),
-    endGroup: jest.fn(),
-    warning: jest.fn(),
-}));
+  getInput: jest.fn(),
+  setFailed: jest.fn(),
+  info: jest.fn(),
+  debug: jest.fn(),
+  startGroup: jest.fn(),
+  endGroup: jest.fn(),
+  warning: jest.fn()
+}))
 jest.mock('@octokit/rest', () => ({
-    Octokit: jest.fn().mockImplementation(() => ({
-        paginate: jest.fn(),
-        rest: {
-            teams: {
-                listMembersInOrg: jest.fn(),
-            },
-            pulls: {
-                listReviews: jest.fn(),
-            },
-            repos: {
-                getContent: jest.fn(),
-            },
-        },
-    })),
-}));
+  Octokit: jest.fn().mockImplementation(() => ({
+    paginate: jest.fn(),
+    rest: {
+      teams: {
+        listMembersInOrg: jest.fn()
+      },
+      pulls: {
+        listReviews: jest.fn()
+      },
+      repos: {
+        getContent: jest.fn()
+      }
+    }
+  }))
+}))
 
 const context = {
-    eventName: 'push',
-    sha: 'fakeSha',
-    ref: 'refs/heads/fake-branch',
-    workflow: 'fakeWorkflow',
-    action: 'fakeAction',
-    actor: 'fakeActor',
-    job: 'fakeJob',
-    runNumber: 123,
-    runId: 123456,
-    payload: {
-        pull_request: {
-            number: 1,
-        }
-    },
-    repo: {
-        owner: 'fakeOwner',
-        repo: 'fakeRepo',
-    },
-    issue: {
-        owner: 'fakeOwner',
-        repo: 'fakeRepo',
-        number: 1,
-    },
-};
-const fakeContext = context as Context;
+  eventName: 'push',
+  sha: 'fakeSha',
+  ref: 'refs/heads/fake-branch',
+  workflow: 'fakeWorkflow',
+  action: 'fakeAction',
+  actor: 'fakeActor',
+  job: 'fakeJob',
+  runNumber: 123,
+  runId: 123456,
+  payload: {
+    pull_request: {
+      number: 1
+    }
+  },
+  repo: {
+    owner: 'fakeOwner',
+    repo: 'fakeRepo'
+  },
+  issue: {
+    owner: 'fakeOwner',
+    repo: 'fakeRepo',
+    number: 1
+  }
+}
+const fakeContext = context as Context
 
 const getContentData = `groups:
     fake-team:
@@ -63,41 +63,47 @@ const getContentData = `groups:
         required: 1`
 
 beforeEach(() => {
-    jest.clearAllMocks();
-});
-
+  jest.clearAllMocks()
+})
 
 describe('run function', () => {
-    it('ok run', async () => {
-        const octokit = new Octokit();
+  it('ok run', async () => {
+    const octokit = new Octokit()
 
-        (octokit.paginate as unknown as jest.Mock).mockImplementation((method, params) => {
-            if (method === octokit.rest.teams.listMembersInOrg) {
-                return Promise.resolve([{ login: 'user1' }, { login: 'user2' }, { login: 'user5' }]);
-            } else if (method === octokit.rest.pulls.listReviews) {
-                return Promise.resolve([
-                    { user: { login: 'user1' }, state: 'APPROVED' },
-                    { user: { login: 'user2' }, state: 'CHANGES_REQUESTED' },
-                    { user: { login: 'user3' }, state: 'APPROVED' },
-                ]);
-            }
-        });
+    ;(octokit.paginate as unknown as jest.Mock).mockImplementation(
+      async method => {
+        if (method === octokit.rest.teams.listMembersInOrg) {
+          return Promise.resolve([
+            { login: 'user1' },
+            { login: 'user2' },
+            { login: 'user5' }
+          ])
+        } else if (method === octokit.rest.pulls.listReviews) {
+          return Promise.resolve([
+            { user: { login: 'user1' }, state: 'APPROVED' },
+            { user: { login: 'user2' }, state: 'CHANGES_REQUESTED' },
+            { user: { login: 'user3' }, state: 'APPROVED' }
+          ])
+        }
+      }
+    )
+    ;(octokit.rest.repos.getContent as unknown as jest.Mock).mockImplementation(
+      async () => {
+        return Promise.resolve({
+          data: {
+            content: Buffer.from(getContentData).toString('base64')
+          }
+        })
+      }
+    )
+    ;(core.setFailed as unknown as jest.Mock).mockImplementation(
+      (message: string | Error) => {
+        console.log(message)
+        return message
+      }
+    )
 
-        (octokit.rest.repos.getContent as unknown as jest.Mock).mockImplementation((() => {
-            return Promise.resolve({
-                data: {
-                    content:
-                        Buffer.from(getContentData).toString('base64')
-                }
-            });
-        }));
-
-        (core.setFailed as unknown as jest.Mock).mockImplementation((message: string | Error) => {
-            console.log(message)
-            return message;
-        });
-
-        await run(fakeContext, octokit, "")
-        expect(core.setFailed).not.toHaveBeenCalled()
-    });
-});
+    await run(fakeContext, octokit, '')
+    expect(core.setFailed).not.toHaveBeenCalled()
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@
  * The entrypoint for the action.
  */
 import * as core from '@actions/core'
-import * as github from '@actions/github';
-import { Octokit } from "@octokit/rest";
+import * as github from '@actions/github'
+import { Octokit } from '@octokit/rest'
 import { run } from './main'
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 
-const token = core.getInput('token');
-const octokit = new Octokit({ auth: token });
-const configPath = core.getInput('config');
-const ctx = github.context;
+const token = core.getInput('token')
+const octokit = new Octokit({ auth: token })
+const configPath = core.getInput('config')
+const ctx = github.context
 
 run(ctx, octokit, configPath)
